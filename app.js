25ccc40e4ff9f5659a31c7786a8774bf9fb96303
Akshat Gupta <akshatvg23@gmail.com>
2020-04-17 02:13:57 +0530

    converted index.jade to index.html and changed routes for the same

--- app.js
+++ app.js
@@ -6,30 +6,39 @@ var nodemailer = require('nodemailer');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
+
+app.get('/', function (req, res) {
 	res.render('index');
 });
 
-app.get('/contact', function(req, res){
+app.get('/index', function (req, res) {
+	res.render('index');
+});
+
+
+app.get('/contact', function (req, res) {
 	res.render('contact');
 });
 
-app.post('/contact/send', function(req, res){
+app.post('/contact/send', function (req, res) {
 	var transporter = nodemailer.createTransport({
 		host: 'smtp.gmail.com',
 		port: 465,
-    	secure: true, // use SSL
+		secure: true, // use SSL
 		auth: {
-			user: 'nodemailer007@gmail.com',//enter email you want to send mail from,
-			pass: 'qddvvqqirwegpjsu'//enter passsword
+			user: 'nodemailer007@gmail.com', //enter email you want to send mail from,
+			pass: 'qddvvqqirwegpjsu' //enter passsword
 		},
-		tls:{
+		tls: {
 			rejectUnauthorized: false,
 		}
 	});
@@ -38,20 +47,20 @@ app.post('/contact/send', function(req, res){
 		from: '"Nodemailer Bot" <nodemailer007@gmail.com>',
 		to: req.body.email,
 		subject: 'Message from Nodemailer',
-		text: 'New Mail! Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
-		html: '<p>New Mail!</p><ul><li>'+req.body.message+'</li></ul>'
+		text: 'New Mail! Name: ' + req.body.name + 'Email: ' + req.body.email + 'Message: ' + req.body.message,
+		html: '<p>New Mail!</p><ul><li>' + req.body.message + '</li></ul>'
 	};
 
-	transporter.sendMail(mailOptions, function(error, info){
-		if(error){
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
 			console.log(error);
 			res.redirect('/');
 		} else {
-			console.log('Message Sent: '+info.response);
+			console.log('Message Sent: ' + info.response);
 			res.redirect('/');
 		}
 	});
 });
-port = process.env.PORT;
+port = process.env.PORT || 3000;
 app.listen(port);
-console.log('Server is running on'+port);
+console.log('Server is running on' + port);
