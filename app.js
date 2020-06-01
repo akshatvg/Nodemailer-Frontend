903b66dc4ff6214d233406ed4c8624b0c934c7b1
Akshat Gupta <akshatvg23@gmail.com>
2020-06-01 21:01:10 +0530

    minor fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
