e2447211dc7920c9f48232b36e9c1307a48c6e15
Shashwat Gupta <shashwat.gupta2019@vitstudent.ac.in>
2020-06-17 02:46:08 +0530

    add mail function 1

--- public/index.js
+++ public/index.js
@@ -1,21 +1,42 @@
-function sendmail(email){
-    var data = {
-        email:email
-    }
-    var xh = new XMLHttpRequest();
-    xh.open("POST", "https://nodemailer-cc.herokuapp.com/send/email", true)
-    xh.setRequestHeader('Content-Type', 'application/json')
-    xh.send(JSON.stringify(data))
+$(document).ready(function(){
+    $('#send1').on('click', function (e) {
+        e.preventDefault();
+        var data = {
+            email : $('input[name="email"]').val(),
+            subject : $('input[name="subject"]').val(),
+            message : $('textarea[name="message"]').val(),
+        }
+        var xh = new XMLHttpRequest();
+        xh.open("POST", "https://nodemailer-cc.herokuapp.com/send/email", true)
+        xh.setRequestHeader('Content-Type', 'application/json')
+        xh.send(JSON.stringify(data))
+        xh.onload = function () {
+            if (this.status == 200) {
+                alert("abc")
+                window.location.replace('/#form')
+            }
+            else if (this.status == 401) {
+                alert("xyz")
+                window.location.replace('/#form-m')
+            }
+        }
+    });
+    
+});
 
-    xh.onload = function () {
-        if (this.status == 200) {
-            alert("abc")
-            window.location.replace('/#form')
-        }
-        else if (this.status == 401) {
-            alert("xyz")
-            window.location.replace('/#form')
-        }
-    }
-};
+
+
+
+// function sendmail(email, subject, message) {
+//     var data = {
+//         email: email
+//         subject:subject
+//     }
+//     
+//     
+//     
+//     
+
+    
+// };
 
