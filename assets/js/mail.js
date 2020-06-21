464ac242a30fefdd236d4a738261f9b7dc607ad7
NavyaaSharma <navyaasharma700@rediffmail.com>
2020-06-21 20:33:03 +0530

    email working

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-    $('#send1').on('click', function (e) {
-        var data = {
-            to : $('input[name="email"]').val(),
-            subject : $('input[name="subject"]').val(),
-            message : $('textarea[name="message"]').val(),
+function email1()
+{
+    var data = {
+            to : document.getElementById('semail').value,
+            subject : document.getElementById('ssubject').value,
+            message : document.getElementById('smsg').value,
         }
         var xh = new XMLHttpRequest();
         xh.open("POST", "https://nodemailer-cc.herokuapp.com/send/email", true)
@@ -12,62 +12,14 @@ $(document).ready(function(){
         xh.onload = function () {
             if (this.status == 200) {
                 swal("Success!", "You mail was successfully sent", "success");
-                window.location.replace('/#form')
+                window.location.replace('index.html')
             }
-            else if (this.status == 401) {
+            else{
                 swal("Unsuccessful", "Please try again", "error");
-                window.location.replace('/index.html#Form')
+                window.location.replace('index.html')
             }
         }
-    });
+    }
     
-
-    $('#send2').on("click",function(e){
-		$('#file').parse({
-			config: {
-				delimiter: "auto",
-				complete: sendmail2,
-			},
-			before: function(file, inputElem)
-			{
-				console.log("Parsing file...", file);
-			},
-			error: function(err, file)
-			{
-				console.log("ERROR:", err, file);
-			},
-			complete: function()
-			{
-				console.log("Done with all files");
-			}
-		});
-    });
-	
-    function sendmail2(results) {
-        
-        var data = {
-            to: results.data,
-            subject: $('input[name="subject1"]').val(),
-            message: $('textarea[name="message1"]').val(),
-            
-        }
-        console.log(data)
-        var xh = new XMLHttpRequest();
-        xh.open("POST", "https://nodemailer-cc.herokuapp.com/send/email", true)
-        xh.setRequestHeader('Content-Type', 'application/json')
-        xh.send(JSON.stringify(data))
-        xh.onload = function () {
-            if (this.status == 200) {
-                swal("Success!", "You mail was successfully sent", "success");
-                window.location.replace('/#form')
-            }
-            else if (this.status == 401) {
-                swal("Unsuccessful", "Please try again", "error");
-                window.location.replace('/#form')
-            }
-        }
-            
-	}
-});
 
 
